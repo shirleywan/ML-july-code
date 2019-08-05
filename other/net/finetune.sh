43636fec65e634a9eb9ed7e7def82eef114cdb32
shirleywan <shirleyti1949@163.com>
2019-08-05 22:54:57 +0800

    Delete finetune.sh

--- other/net/finetune.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-TOOLS=./build/tools
-
-$TOOLS/caffe train \
-    --solver=CIFAR-10/finetune_solver.prototxt \
-    --weights=CIFAR-10/cifar10_iter_70000.caffemodel #\
-    #>>CIFAR-10/log.txt 2>&1 
