f27f52392cefc7be307a2dddf2e3ee46e6e35b5f
shirleywan <shirleyti1949@163.com>
2019-08-05 23:04:48 +0800

    Delete string_split.py

--- other/string_split.py
+++
@@ -1,6 +0,0 @@
-from textwrap import wrap
-a = 'urivthvtlqqerctlxmjvkgvfclaaduwmaadedpadanl'
-a='123456'
-b = 'batkqdhjnrwtsmzidswdnenqpsblsszldyttytrgenaizwehntqiaaufble'
-print '\n'.join(wrap('0'*(4-len(a)%4)+a, 4))
-# print '\n'.join(wrap(b+'0'*(4-len(b)%4), 4))
