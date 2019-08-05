e71fd299daeb1e9cae7eb75306334f77c586d8a4
shirleywan <shirleyti1949@163.com>
2019-08-05 22:57:59 +0800

    Delete train.sh

--- other/net/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-GLOG_logtostderr=0 GLOG_log_dir=models/ped_attributes_V_3.7/Log/ ./build/tools/caffe train --solver=models/ped_attributes_V_3.7/solver.prototxt --weights=/home/peiwenfang/caffe-master/VGG_CNN_M_1024.v2.caffemodel -gpu=0
